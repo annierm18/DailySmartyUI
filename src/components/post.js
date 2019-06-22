f77cf9fbf41abe37e138801c86fbf906e9f77fab
annierm18 <annierm18@yahoo.com>
2019-06-21 18:23:44 -0700

    modifed fetchpostswithquery actionCreator to take in an optional callbak

--- src/components/post.js
+++ src/components/post.js
@@ -15,7 +15,7 @@ class Post extends Component {
                 <div className="post-link" key={index}>
                     <div className="post-link__box"></div>
                     <div className="post-link__link">
-                        <a href={post_link.link_urll}>Useful Link #{index + 1}</a>
+                        <a href={post_link.link_url}>Useful Link #{index + 1}</a>
                     </div> 
                 </div>
            )
